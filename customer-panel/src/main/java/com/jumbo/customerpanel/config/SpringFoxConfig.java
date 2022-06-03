0ffd8ffbb4cc74548351ca101c217c10b611e1f6
erfan.golduzian <erfan.golduzian@gmail.com>
2022-06-03 16:13:27 +0430

    feat (swagger API)  add and setup swagger API, it is available in this address  ../customer-panel/swagger-ui/

---
+++ customer-panel/src/main/java/com/jumbo/customerpanel/config/SpringFoxConfig.java
@@ -0,0 +1,21 @@
+package com.jumbo.customerpanel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                //exclude error for base-error-controller for spring
+                .apis(requestHandler -> requestHandler.groupName().contains("controller")
+                        && !requestHandler.groupName().contains("error"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
